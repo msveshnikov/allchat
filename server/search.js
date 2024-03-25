df399cc8bdcb5b1d25be6eab721f17450adeb1f9
Max Sveshnikov <msveshnikov@gmail.com>
2024-03-25 21:03:48 +0100

    More

--- server/search.js
+++ server/search.js
@@ -1,8 +1,23 @@
 import puppeteer from "puppeteer";
+import { JSDOM } from "jsdom";
+
+const userAgents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:57.0) Gecko/20100101 Firefox/57.0",
+];
 
 export async function fetchSearchResults(query) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        product: "firefox",
+        executablePath: "/usr/bin/firefox-esr",
+    });
     const page = await browser.newPage();
+    const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+    await page.setUserAgent(randomUserAgent);
 
     // Navigate to Google Search
     const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
@@ -21,4 +36,23 @@ export async function fetchSearchResults(query) {
 
     await browser.close();
     return extractedData;
+}
+
+export async function fetchPageContent(url) {
+    const browser = await puppeteer.launch({
+        product: "firefox",
+        executablePath: "/usr/bin/firefox-esr",
+    });
+    const page = await browser.newPage();
+    const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+    await page.setUserAgent(randomUserAgent);
+
+    await page.goto(url, { waitUntil: "networkidle2" });
+    const html = await page.content();
+
+    const dom = new JSDOM(html);
+    const content = dom.window.document.documentElement.textContent;
+
+    await browser.close();
+    return content;
 }
