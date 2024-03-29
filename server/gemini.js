65e83a61325b1a7c8dd7175113fefa4385a4b76f
Max Sveshnikov <msveshnikov@gmail.com>
2024-03-29 13:28:47 +0100

    Refactor

--- server/gemini.js
+++ server/gemini.js
@@ -4,14 +4,13 @@ import stream from "stream";
 dotenv.config({ override: true });
 
 const model = "gemini-1.5-pro-latest";
+const GENAI_DISCOVERY_URL = `https://generativelanguage.googleapis.com/$discovery/rest?version=v1beta&key=${process.env.GEMINI_KEY}`;
 
-const URL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${process.env.GEMINI_KEY}`;
 export const getTextGemini = async (prompt, temperature) => {
-    const headers = {
-        "Content-Type": "application/json",
-    };
+    const genaiService = await google.discoverAPI({ url: GENAI_DISCOVERY_URL });
+    const auth = new google.auth.GoogleAuth().fromAPIKey(process.env.GEMINI_KEY);
 
-    const data = {
+    const contents = {
         contents: [
             {
                 role: "user",
@@ -35,22 +34,15 @@ export const getTextGemini = async (prompt, temperature) => {
         },
     };
 
-    const response = await fetch(URL, {
-        method: "POST",
-        headers,
-        body: JSON.stringify(data),
+    const generateContentResponse = await genaiService.models.generateContent({
+        model: `models/${model}`,
+        requestBody: contents,
+        auth: auth,
     });
 
-    if (!response.ok) {
-        console.error(response.statusText);
-        throw new Error("Gemini Error:" + response.statusText);
-    }
-
-    const result = await response.json();
-    return result?.candidates?.[0]?.content?.parts?.[0]?.text;
+    return generateContentResponse?.data?.candidates?.[0]?.content?.parts?.[0]?.text;
 };
 
-const GENAI_DISCOVERY_URL = `https://generativelanguage.googleapis.com/$discovery/rest?version=v1beta&key=${process.env.GEMINI_KEY}`;
 export async function getTextVision(prompt, imageBase64) {
     const genaiService = await google.discoverAPI({ url: GENAI_DISCOVERY_URL });
     const auth = new google.auth.GoogleAuth().fromAPIKey(process.env.GEMINI_KEY);
@@ -75,8 +67,15 @@ export async function getTextVision(prompt, imageBase64) {
     const contents = {
         contents: [
             {
+                role: "user",
                 parts: [{ text: prompt }, { file_data: { file_uri: fileUri, mime_type: file.mimeType } }],
             },
+        ],
+        safetySettings: [
+            { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_NONE" },
+            { category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_NONE" },
+            { category: "HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold: "BLOCK_NONE" },
+            { category: "HARM_CATEGORY_DANGEROUS_CONTENT", threshold: "BLOCK_NONE" },
         ],
     };
 
