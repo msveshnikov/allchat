8c5e57e9bc46f69e02174e62dd17d516c160da7f
Max Sveshnikov <msveshnikov@gmail.com>
2024-04-23 18:42:01 +0200

    markdown in PDF

--- src/components/pdfGenerator.js
+++ src/components/pdfGenerator.js
@@ -1,7 +1,11 @@
 import * as pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
+import Markdown from "markdown-it";
+import htmlToPdfmake from "html-to-pdfmake";
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+const md = new Markdown();
 
 const styles = {
     userMessage: {
@@ -9,18 +13,18 @@ const styles = {
         bold: true,
         alignment: "right",
         margin: [0, 10, 0, 5],
-        backgroundColor: "#e8f5e9", // Light green background for assistant messages
-        color: "#33691e", // Dark green text color for assistant messages
-        padding: [5, 10], // Add some padding for better readability
-        borderRadius: 5, // Round the corners of the bubble
+        backgroundColor: "#e8f5e9",
+        color: "#33691e",
+        padding: [5, 10],
+        borderRadius: 5,
     },
     assistantMessage: {
         fontSize: 12,
         margin: [0, 5, 0, 10],
-        backgroundColor: "#e1f5fe", // Light blue background for user messages
-        color: "#01579b", // Dark blue text color for user messages
-        padding: [5, 10], // Add some padding for better readability
-        borderRadius: 5, // Round the corners of the bubble
+        backgroundColor: "#e1f5fe",
+        color: "#01579b",
+        padding: [5, 10],
+        borderRadius: 5,
     },
 };
 
@@ -36,7 +40,7 @@ const generatePdfDocDefinition = (chatHistory) => {
             if (chat.userImageData && (chat.fileType === "png" || chat.fileType === "jpeg")) {
                 content.push({
                     image: `data:image/${chat.fileType};base64,` + chat.userImageData,
-                    width: 500,
+                    width: 400,
                 });
             }
             content.push({ text: chat.user, style: "userMessage" });
@@ -48,12 +52,14 @@ const generatePdfDocDefinition = (chatHistory) => {
             if (chat.image) {
                 const images = Array.isArray(chat.image)
                     ? chat.image.map((imgData) => {
-                          return { image: "data:image/png;base64," + imgData, width: 500 };
+                          return { image: "data:image/png;base64," + imgData, width: 400 };
                       })
-                    : [{ image: "data:image/png;base64," + chat.image, width: 500 }];
+                    : [{ image: "data:image/png;base64," + chat.image, width: 400 }];
                 content.push(...images);
             }
-            content.push({ text: chat.assistant, style: "assistantMessage" });
+            const renderedMarkdown = md.render(chat.assistant);
+            const pdfmakeContent = htmlToPdfmake(renderedMarkdown);
+            content.push(pdfmakeContent);
             docDefinition.content.push(...content);
         }
     }
