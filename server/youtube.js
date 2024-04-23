54048dd6493fd5b9f8656b5ad8bf8757fe961dab
Max Sveshnikov <msveshnikov@gmail.com>
2024-04-23 08:28:20 +0200

    Fix YouTube summary

--- server/youtube.js
+++ server/youtube.js
@@ -1,39 +1,11 @@
-import { google } from "googleapis";
+import { YoutubeTranscript } from "youtube-transcript";
 import { getTextClaude } from "./claude.js";
-import dotenv from 'dotenv';
-dotenv.config();
-
-const youtube = google.youtube({
-    version: "v3",
-    auth: process.env.YOUTUBE_KEY,
-});
 
 export const summarizeYouTubeVideo = async (videoId) => {
     try {
-        const captionResponse = await youtube.captions.list({
-            videoId,
-            part: "snippet",
-        });
-
-        const captions = captionResponse.data.items
-            .map(
-                (item) =>
-                    item.snippet.trackKind === "ASR" && item.snippet.language === "en" && item.snippet.isTranslatable
-            )
-            .filter(Boolean)
-            .map((item) => item.id);
-
-        if (!captions.length) {
-            return "No captions found for this video";
-        }
-
-        const captionTracksResponse = await youtube.captions.download({
-            videoId,
-            id: captions,
-            tfmt: "srt",
-        });
-
-        const captionText = Buffer.from(captionTracksResponse.data).toString("utf-8");
+        const script = await YoutubeTranscript.fetchTranscript(videoId);
+        const captionText = script.map((s) => s.text).join(" ");
+        console.log(captionText);
         const summary = await getTextClaude(
             `Summarize the following in 100 words:\n\n${captionText}`,
             0.5,
