68dec1b2e2ab43924b6ebb40cd10a81fc794249a
Max Sveshnikov <msveshnikov@gmail.com>
2024-05-14 14:32:41 +0200

    Update scheduler.js

--- server/scheduler.js
+++ server/scheduler.js
@@ -17,10 +17,10 @@ export const scheduleAction = async (action, schedule, userId) => {
             const result = await getTextGpt(
                 `User information: ${userInfo} Please execute user requested action (do the task, don't request it but do YOURSELF): ${action}`,
                 0.2,
+                null,
+                null,
                 userId,
                 "gpt-3.5-turbo",
-                null,
-                null,
                 true
             );
             const actionTimestamp = Date.now();
