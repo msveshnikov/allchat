0c6ef4dfdbb2c19f9e42544caa2fc903108ddc87
Max Sveshnikov <msveshnikov@gmail.com>
2024-07-27 10:46:06 +0200

    Minor

--- src/components/UserCountriesPieChart.js
+++ src/components/UserCountriesPieChart.js
@@ -70,7 +70,7 @@ const UserCountriesPieChart = ({ usersByCountry }) => {
                         outerRadius={130}
                         fill="#8884d8"
                         dataKey="value"
-                        label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+                        // label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
                     >
                         {pieData.map((entry, index) => (
                             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
